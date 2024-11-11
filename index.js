3387cbc828abf346d2929b68676ec7b2298a83c6
Nicolás Flores <110703674+nicolasezequielflores@users.noreply.github.com>
2024-11-10 22:25:48 -0300

    Validación terminada

--- index.js
+++ index.js
@@ -1,66 +1,95 @@
 const d = document;
 const btn = d.querySelector('.btn');
 const cardContainer = d.querySelector('.cardContainer');
+const entrada = d.getElementById('entrada');
+const validacion = d.querySelector('.val');
+let timeOut;
 
 //Eventos
 btn.addEventListener('click', MostrarTexto);
+entrada.addEventListener('input', Validacion);
+
+//Funcion para mostrar un mensaje de validacion cuando el text area este vacio
+function Validacion(){
+    
+    //Validación del textarea
+    if(entrada.value.trim() !== ''){
+        validacion.className = 'val';
+        clearTimeout(timeOut); //Limpia el setTimeout si existe.
+    }
+
+    else{
+        validacion.className = 'val__vis';
+        clearTimeout(timeOut); //Limpia el setTimeout si existe.
+        timeOut = setTimeout(() =>{
+            validacion.className = 'val';
+        }, 5000);
+    }
+}
 
 //Funcion para mostar texto.
 function MostrarTexto(){
 
-    //CAPTURO EL TEXTO DEL Textarea Y SE LO PASO A LA CARD.
-    const texto = d.getElementById('entrada').value;
-    d.getElementById('entrada').value = ''; //Dejar el textarea vacio.
+    //VALIDACION
+    if(entrada.value.trim() !== ''){
+        //CAPTURO EL TEXTO DEL Textarea Y SE LO PASO A LA CARD.
+        const texto = d.getElementById('entrada').value;
+        d.getElementById('entrada').value = ''; //Dejar el textarea vacio.
 
-    const cards = d.createElement('div');
-    cards.classList = 'cards';
+        //Muestro el texto del input a la card
+        const cards = d.createElement('div');
+        cards.classList = 'cards';
 
-    const nuevaCard = d.createElement('div');
-    nuevaCard.classList = 'card';
-    nuevaCard.innerHTML = texto;
+        const nuevaCard = d.createElement('div');
+        nuevaCard.classList = 'card';
+        nuevaCard.innerHTML = texto;
 
-    cards.appendChild(nuevaCard)
-    cardContainer.appendChild(cards);
+        cards.appendChild(nuevaCard)
+        cardContainer.appendChild(cards);
 
-    //BOTON DE ELIMINAR.
-    const btns = d.createElement('div');
-    btns.classList = 'btns'
+        //BOTON DE ELIMINAR.
+        const btns = d.createElement('div');
+        btns.classList = 'btns'
 
-    const btnEliminar = d.createElement('a');
-    btnEliminar.classList = 'btn__eliminar';
-    btnEliminar.innerHTML = 'Eliminar';
+        const btnEliminar = d.createElement('a');
+        btnEliminar.classList = 'btn__eliminar';
+        btnEliminar.innerHTML = 'Eliminar';
 
-    btns.appendChild(btnEliminar)
-    cardContainer.appendChild(btns);
+        btns.appendChild(btnEliminar)
+        cardContainer.appendChild(btns);
 
-    btnEliminar.addEventListener('click', EliminarCard);
+        btnEliminar.addEventListener('click', EliminarCard);
 
-    //BOTON DE COMPLETADO.
-    const btnCompletado = d.createElement('a');
-    btnCompletado.classList = 'btn__completado';
-    btnCompletado.innerHTML = 'Completado';
-    
-    btns.appendChild(btnCompletado)
-    cardContainer.appendChild(btns);
+        //BOTON DE COMPLETADO.
+        const btnCompletado = d.createElement('a');
+        btnCompletado.classList = 'btn__completado';
+        btnCompletado.innerHTML = 'Completado';
+        
+        btns.appendChild(btnCompletado)
+        cardContainer.appendChild(btns);
 
-    btnCompletado.addEventListener('click', CompletadoCard);
+        btnCompletado.addEventListener('click', CompletadoCard);
 
-    //Funcion para eliminar card.
-    function EliminarCard(){
-        if(cards && cards.parentNode && btns && btns.parentNode){
-            cards.parentNode.removeChild(cards);
-            btns.parentNode.removeChild(btns);
+        
+        //Funcion para eliminar card.
+        function EliminarCard(){
+            if(cards && cards.parentNode && btns && btns.parentNode){
+                cards.parentNode.removeChild(cards);
+                btns.parentNode.removeChild(btns);
+            }
+            else{
+                console.log('El elemento no existe');
+            }
         }
-        else{
-            console.log('El elemento no existe');
+
+        //Funcion para marcar como completado.
+        function CompletadoCard(){
+            nuevaCard.classList.toggle('card__completado');
+            btnCompletado.innerHTML = (btnCompletado.innerHTML === 'Completado') ? 'No completado' : 'Completado';
         }
     }
-
-    //Funcion para marcar como completado.
-    function CompletadoCard(){
-        nuevaCard.classList.toggle('card__completado');
-        
-        btnCompletado.innerHTML = (btnCompletado.innerHTML === 'Completado') ? 'No completado' : 'Completado';
+    else{
+        return
     }
 
 }
